13dcf85413eb2f2ebdfbc14a70c05479db7e0652
justvmexit <00201378055946876892@protonmail.com>
2021-11-12 20:22:55 +0100

    Better frontpage

--- dashboard/src/views/Home.vue
+++ dashboard/src/views/Home.vue
@@ -1,14 +1,32 @@
 <template>
   <div>
-    <h1> Hello homie </h1>
+    <h1> Welcome to the MVP project </h1>
+    <p id="content">
+      An open-source authentication system written in modern JavaScript and C++ 17.
+      We focus on maintaining security and we only encrypt actual images with keys derived from license.
+      We follow one rule.<br>
+      <span>NEVER TRUST THE CLIENT</span>
+    </p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
-  components: {
-    
-  }
 }
 </script>
+
+<style scoped>
+  #content {
+    width: 375px;
+    margin: auto;
+  }
+
+  p {
+    word-wrap: break-word;
+  }
+
+  p span {
+    font-size: 40px;
+  }
+</style>
